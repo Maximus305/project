96fe37c11de6512090f7b687096f6c83fbef11dc
Maximus305 <b7ueshark@iCloud.com>
2024-10-17 20:42:47 -0500

    packege attempted to fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
       </main>
 
       <footer className="container mx-auto py-6 px-4 text-center">
-        <p>&copy; 2024 PDF2LLM Converter. All rights reserved.</p>
+        <p>&copy; 2025 PDF2LLM Converter. All rights reserved.</p>
       </footer>
       
       <style jsx>{`
